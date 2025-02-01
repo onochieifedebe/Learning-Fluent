02e7afbfa6099eba9024cf95479e81f9c5504be1
Onochie Ifedebe <admin@example.com>
2025-02-01 00:43:39 -0500

    Updated Client Script

--- src/fluent/index.now.ts
+++ src/fluent/index.now.ts
@@ -1,42 +1,56 @@
 import '@servicenow/sdk/global'
-import { BusinessRule, ChoiceColumn, ClientScript, IntegerColumn, ReferenceColumn, StringColumn, Table } from '@servicenow/sdk/core'
+import {
+    BusinessRule,
+    ChoiceColumn,
+    ClientScript,
+    IntegerColumn,
+    ReferenceColumn,
+    StringColumn,
+    Table,
+} from '@servicenow/sdk/core'
 import { showStateUpdate } from '../server/script.js'
-
 
 export const x_1387810_other_fl_service_approval = Table({
     name: 'x_1387810_other_fl_service_approval',
     label: 'Service Approval',
     schema: {
         u_order: IntegerColumn({
-            label: 'Order'
+            label: 'Order',
         }),
-        u_source: ChoiceColumn({label: "Approval Source",
+        u_source: ChoiceColumn({
+            label: 'Approval Source',
             choices: {
                 decision_table: {
                     label: 'Decision Table',
                     sequence: 1,
-                    inactive: false
+                    inactive: false,
+                    inactive_on_update: false,
                 },
                 funding_profile: {
                     label: 'Funding Profile',
                     sequence: 2,
-                    inactive: false
+                    inactive: false,
+                    inactive_on_update: false,
                 },
                 entitlement: {
                     label: 'Entitlement',
                     sequence: 3,
-                    inactive: false
-                }
-            }
+                    inactive: false,
+                    inactive_on_update: false,
+                },
+            },
         }),
         u_approver: ReferenceColumn({
             label: 'Approver',
-            referenceTable: 'sys_user'
+            referenceTable: 'sys_user',
+            attributes: {},
+            maxLength: 32,
         }),
         u_approval_type: StringColumn({
-            label: "Approval Type"
-        })
-    }
+            label: 'Approval Type',
+        }),
+    },
+    index: [{ name: 'index', element: ['u_approver'], unique: false }],
 })
 
 //creates a client script that pops up 'Table loaded successfully!!' message every time todo record is loaded
@@ -67,6 +81,7 @@ BusinessRule({
     order: 100,
     when: 'after',
     active: true,
+    abort_action: false,
 })
 
 ClientScript({
@@ -76,7 +91,23 @@ ClientScript({
     type: 'onChange',
     field: 'u_source',
     ui_type: 'all',
-    active: false,
-    description: "Only shows the Approval Type field when the Approval Source is Decision Table",
-    isolate_script: false
+    active: true,
+    description: 'Only shows the Approval Type field when the Approval Source is Decision Table',
+    isolate_script: false,
+    script: script`
+             function onChange(control, oldValue, newValue, isLoading, isTemplate) {
+                if (isLoading) {
+                   return;
+                }
+             
+                if (newValue == 'u_decision_table'){
+             	g_form.setVisible('u_approval_type', true);
+                }
+                else {
+             	g_form.setVisible('u_approval_type', false);
+                }
+             
+                //Type appropriate comment here, and begin script below
+                
+             }`,
 })
